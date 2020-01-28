fdd49a688d32817b5cc7110d4c02d3002ccd3c40
Rishikesh <rishikesh1419@gmail.com>
2020-01-28 22:21:04 +0530

    Updated RMI client

--- RMI/CalculatorClient.java
+++ RMI/CalculatorClient.java
@@ -1,19 +1,27 @@
 import java.rmi.Naming; 
 import java.rmi.RemoteException; 
 import java.net.MalformedURLException; 
-import java.rmi.NotBoundException; 
+import java.rmi.NotBoundException;
+import java.util.Scanner;
  
 public class CalculatorClient { 
  
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+	int a,b;
+	Scanner sc = new Scanner(System.in);
         try { 
             Calculator c = (Calculator)
                            Naming.lookup(
                  "rmi://localhost/CalculatorService"); 
-            System.out.println( c.sub(4, 3) ); 
-            System.out.println( c.add(4, 5) ); 
-            System.out.println( c.mul(3, 6) ); 
-            System.out.println( c.div(9, 3) ); 
+	    System.out.println("Enter numbers A and B (A>B):");
+	    System.out.print("A = ");
+	    a = sc.nextInt();
+	    System.out.print("B = ");
+	    b = sc.nextInt();
+	    System.out.println( c.add(a,b) );
+            System.out.println( c.sub(a,b) );
+            System.out.println( c.mul(a,b) );
+            System.out.println( c.div(a,b) );
         } 
         catch (MalformedURLException murle) { 
             System.out.println(); 
